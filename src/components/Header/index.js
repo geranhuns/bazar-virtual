86b65da1b0d4b8ea96da3505e7619073b1c62e2d
geranhuns <gera.nuncio@gmail.com>
2024-04-08 19:24:53 -0600

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Logo from "./Logo";
+import Menu from "./Menu";
+export default function Header() {
+  return (
+    <header>
+      <Logo />
+      <Menu />
+    </header>
+  );
+}
