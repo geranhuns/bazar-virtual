459adf60294f09220d355c2c5b9537472a7103c3
geranhuns <gera.nuncio@gmail.com>
2024-04-16 21:36:36 -0600

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,14 @@
+import Logo from "../Logos/Logo";
 export default function Footer() {
   return (
-    <footer>
-      <div class="footer-content">
-        <div class="footer-logo">
-          <img src="ruta_a_tu_logo.png" alt="Logo de tu marca" />
+    <footer className="text-center absolute bottom-0 w-full bg-gray-100">
+      <div className="footer-content">
+        <div className="footer-logo flex justify-center">
+          <Logo width="100" />
         </div>
-        <div class="footer-contact-info">
-          <p>Dirección: [Dirección de tu empresa]</p>
-          <p>Teléfono: [Número de teléfono]</p>
-          <p>
-            Correo electrónico:{" "}
-            <a href="mailto:correo@tudominio.com">correo@tudominio.com</a>
-          </p>
-        </div>
-        <div class="footer-social">
-          <p>Síguenos en redes sociales:</p>
-          {/* <!-- Aquí puedes añadir los iconos de tus redes sociales con enlaces --> */}
-          <a href="#">
-            <img src="ruta_a_icono_facebook.png" alt="Facebook" />
-          </a>
-          <a href="#">
-            <img src="ruta_a_icono_twitter.png" alt="Twitter" />
-          </a>
-          <a href="#">
-            <img src="ruta_a_icono_instagram.png" alt="Instagram" />
-          </a>
-        </div>
-        <div class="footer-links">
-          <ul>
+
+        <div className="footer-links">
+          <ul className="flex justify-center gap-7 list-disc">
             <li>
               <a href="#">Acerca de nosotros</a>
             </li>
@@ -43,8 +24,8 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div class="footer-copyright">
-        <p>Derechos de autor © [Año] [Nombre de tu empresa]</p>
+      <div className="footer-copyright">
+        <p>Derechos de autor © 2024 Bazar Virtual - Consume Local</p>
       </div>
     </footer>
   );
