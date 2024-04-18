5f062b151c579fc732abd1fe99fe4e1d06575b38
geranhuns <gera.nuncio@gmail.com>
2024-04-18 11:43:22 -0600

    added basic info to home related to bazares

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex mt-28 justify-around">
+      <div className="advertising-left ">
+        <ul>
+          <li>Shopping</li>
+          <li>Productos Mexicanos</li>
+          <li>Consume Local</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Próximos Bazares</h3>
+        <button>Jalisco</button>
+        <ul>
+          <li>Morena Bazar - 10 jun</li>
+          <li>Mil Cosas Bazar - 17 jun</li>
+          <li>Bazar Manos Mexicanas - 24 jun</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
